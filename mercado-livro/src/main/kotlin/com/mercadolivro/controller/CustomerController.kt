3180f392b0240a451b72da625f24062ccfa27a62
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-01-27 22:43:49 -0300

    @leticiazimerer

---
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,21 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun getCustomer(): CustomerModel {
+        return CustomerModel("1", "Gustavo", "email@email.com")
+    }
+
+    @PostMapping
+    fun createCustomer(@RequestBody customer: PostCustomerRequest) {
+        println(customer)
+    }
+
+}
