980a65e0cb3b11dc7fc26d8b156ce3d69622dd25
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-02-19 20:55:08 -0300

    feat(): Conecta com o Bando de dados MySQL

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -19,9 +19,12 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
