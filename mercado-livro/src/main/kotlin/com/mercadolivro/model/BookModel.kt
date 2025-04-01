7ebcce9d9baf8c192d115a15a06213d31e2cdfa9
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-04-01 20:50:55 -0300

    feat(): Bloqueia alteração de status de livros cancelados/deletados

--- mercado-livro/src/main/kotlin/com/mercadolivro/model/BookModel.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/model/BookModel.kt
@@ -17,11 +17,29 @@ data class BookModel(
     @Column
     var price: BigDecimal,
 
-    @Column
-    @Enumerated(EnumType.STRING) // O status do livro é armazenado como texto no banco.
-    var status: BookStatus? = null,
+
 
     @ManyToOne // muitos livros para 1 usuário
     @JoinColumn(name = "customer_id")
     var customer: CustomerModel? = null // O livro pode ou não ter um cliente associado (null) e se um cliente comprar o livro, essa propriedade será preenchida com um CustomerModel, representando o comprador.
-)
+) {
+    // Como sobrescrever um valor:
+    @Column
+    @Enumerated(EnumType.STRING) // O status do livro é armazenado como texto no banco.
+    var status: BookStatus? = null
+        set(value) { // novo valor que vamos utilizar
+            if (field == BookStatus.DELETADO || field == BookStatus.CANCELADO) { // field é o valor atual do nosso campo
+                throw Exception("Não é possivel alterar um livro com o status ${field}")
+            }
+            field = value // caso nao seja deletado e nem cancelado, ele deixa alterar o status
+        }
+
+    // como tiramos o status de cima, precisa ter um construtor
+    constructor(id : Int? = null,
+                name : String,
+                price: BigDecimal,
+                customer: CustomerModel? = null,
+                status: BookStatus?) : this(id, name, price, customer) { // "this(id, name, price, customer)" invoca o construtor que tem em cima
+                    this.status = status
+                }
+}
