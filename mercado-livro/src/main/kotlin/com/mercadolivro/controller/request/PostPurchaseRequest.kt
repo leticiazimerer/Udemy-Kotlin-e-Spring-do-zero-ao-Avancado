0b6bcb1812a9bfd8c0107329f724f84d86f5ed84
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-05-24 14:47:40 -0300

    feat(): Add JsonAlias

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import jakarta.validation.constraints.Positive
 import org.jetbrains.annotations.NotNull
 
@@ -7,8 +8,10 @@ data class PostPurchaseRequest(
 
     @field:NotNull
     @field: Positive // Garante que o valor é positivo
+    @JsonAlias("customer_id") // Permite que o campo seja recebido como "customer_id" no JSON)
     val customerId: Int,
 
     @field:NotNull
+    @JsonAlias("book_ids")
     val bookIds: Set<Int> // Usamos Set para receber apenas valores diferentes
 )
