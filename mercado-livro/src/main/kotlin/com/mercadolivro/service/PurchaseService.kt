9297a1647e3123126ebf211196860fad853d6cf8
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-05-27 20:40:08 -0300

    feat(): Add validação de vender livros apenas com status.ACTIVE

--- mercado-livro/src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
@@ -1,6 +1,9 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.enums.Errors
 import com.mercadolivro.events.PurchaseEvent
+import com.mercadolivro.extension.BadRequestException
 import com.mercadolivro.model.PurchaseModel
 import com.mercadolivro.repository.PurchaseRepository
 import org.springframework.context.ApplicationEventPublisher
@@ -9,14 +12,23 @@ import org.springframework.stereotype.Service
 @Service
 class PurchaseService(
     private val purchaseRepository: PurchaseRepository,
-    private val applicationEventPublisher: ApplicationEventPublisher
+    private val applicationEventPublisher: ApplicationEventPublisher,
 ) {
+
     fun create(purchaseModel: PurchaseModel) {
+        val invalidBooks = purchaseModel.books.filter { it.status != BookStatus.ATIVO } // Filtra os livros que não estão ativos
+        if (invalidBooks.isNotEmpty()) { // Se houver livros inválidos, lança uma exceção
+            throw BadRequestException(
+                Errors.ML102.message.format(invalidBooks.joinToString { it.status?.name ?: "UNKNOWN" }), // Formata a mensagem de erro com os status dos livros inválidos
+                Errors.ML102.code
+            )
+        }
+
         purchaseRepository.save(purchaseModel)
-        applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel)) // Dispara o evento de compra após salvar o modelo de compra no repositório
+        applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel))
     }
 
     fun update(purchaseModel: PurchaseModel) {
-        purchaseRepository.save(purchaseModel) // Atualiza o modelo de compra no repositório
+        purchaseRepository.save(purchaseModel)
     }
 }
