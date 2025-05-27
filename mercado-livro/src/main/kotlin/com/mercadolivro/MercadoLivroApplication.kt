78c9ac68e9596d22bc23a51692e998f9a7ad563b
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-05-27 20:07:31 -0300

    feat(): Deixa os eventos assíncronos

--- mercado-livro/src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
@@ -2,7 +2,9 @@ package com.mercadolivro
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync // Habilita a execução assíncrona de métodos anotados com @Async (nesse caso eventos)
 @SpringBootApplication
 class MercadoLivroApplication
 
--- mercado-livro/src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
@@ -4,11 +4,13 @@ package com.mercadolivro.events.listener
     import com.mercadolivro.service.PurchaseService
     import java.util.UUID
     import org.springframework.context.event.EventListener
+    import org.springframework.scheduling.annotation.Async
     import org.springframework.stereotype.Component
 
     @Component
     class GenerateNfeListener(private val purchaseService: PurchaseService) {
 
+        @Async
         @EventListener
         fun listen(purchaseEvent: PurchaseEvent) {
             val nfe = UUID.randomUUID().toString() // Gera um UUID aleatório como string
