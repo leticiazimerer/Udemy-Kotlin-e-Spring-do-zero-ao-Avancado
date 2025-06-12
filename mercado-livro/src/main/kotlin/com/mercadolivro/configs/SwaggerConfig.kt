f1bf77e2ddc5f797cff8ed67c107b00e90462361
Leticia Zimerer <leticiazimerer@yahoo.com.br>
2025-06-11 21:02:47 -0300

    feat(): spring profiles

--- mercado-livro/src/main/kotlin/com/mercadolivro/configs/SwaggerConfig.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/configs/SwaggerConfig.kt
@@ -2,6 +2,7 @@ package com.mercadolivro.configs
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
@@ -9,6 +10,7 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@Profile("!prod") // Ativa o Swagger apenas em ambientes que não sejam de produção
 @Configuration
 @EnableSwagger2 // Anotação que ativa o Swagger na aplicação
 class SwaggerConfig {
